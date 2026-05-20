3de96866db6fe6d2802d506bf3623af801a7f98e
adree3 <adrian200yy@gmail.com>
2026-05-20 18:12:04 +0200

    feat(auditoria): restringir acceso al endpoint solo para roles ADMIN y SUPER_ADMIN

--- src/main/java/com/example/quiropracticoapi/controller/AuditoriaController.java
+++ src/main/java/com/example/quiropracticoapi/controller/AuditoriaController.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
@@ -19,6 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/auditoria")
+@PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
 public class AuditoriaController {
 
     private final AuditoriaServiceImpl auditoriaServiceImpl;
